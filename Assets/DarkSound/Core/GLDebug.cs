031cb06fba99980253786de544c8830f2c5f80ee
josephwhi98 <joseph.whitehead101@gmail.com>
2021-05-12 12:52:24 +0100

    Clean up of unused assets

--- Assets/DarkSound/Core/GLDebug.cs
+++ Assets/DarkSound/Core/GLDebug.cs
@@ -1,5 +1,3 @@
-// http://www.unity3d-france.com/unity/phpBB3/viewtopic.php?f=24&t=5409
-
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -41,14 +39,9 @@ public class GLDebug : MonoBehaviour
 
     public KeyCode toggleKey;
     public bool displayLines = true;
-#if UNITY_EDITOR
-    public bool displayGizmos = true;
-#endif
-    //public ScreenRect rect = new ScreenRect (0, 0, 150, 20);
 
     private List<Line> linesZOn;
     private List<Line> linesZOff;
-    //        private float milliseconds;
 
     void Awake()
     {
@@ -83,44 +76,15 @@ public class GLDebug : MonoBehaviour
 
         if (!displayLines)
         {
-            //                        Stopwatch timer = Stopwatch.StartNew ();
-
             linesZOn = linesZOn.Where(l => !l.DurationElapsed(false)).ToList();
             linesZOff = linesZOff.Where(l => !l.DurationElapsed(false)).ToList();
-
-            //                        timer.Stop ();
-            //                        milliseconds = timer.Elapsed.Ticks / 10000f;
         }
     }
-
-    /*void OnGUI ()
-    {
-            GUI.Label (rect, "GLDebug : " + milliseconds.ToString ("f") + " ms");
-    }*/
-
-#if UNITY_EDITOR
-    void OnDrawGizmos()
-    {
-        if (!displayGizmos || !Application.isPlaying)
-            return;
-        for (int i = 0; i < linesZOn.Count; i++)
-        {
-            Gizmos.color = linesZOn[i].color;
-            Gizmos.DrawLine(linesZOn[i].start, linesZOn[i].end);
-        }
-        for (int i = 0; i < linesZOff.Count; i++)
-        {
-            Gizmos.color = linesZOff[i].color;
-            Gizmos.DrawLine(linesZOff[i].start, linesZOff[i].end);
-        }
-    }
-#endif
 
     void OnPostRender()
     {
-        if (!displayLines) return;
-
-        //            Stopwatch timer = Stopwatch.StartNew ();
+        if (!displayLines) 
+            return;
 
         matZOn.SetPass(0);
         GL.Begin(GL.LINES);
@@ -131,9 +95,6 @@ public class GLDebug : MonoBehaviour
         GL.Begin(GL.LINES);
         linesZOff = linesZOff.Where(l => !l.DurationElapsed(true)).ToList();
         GL.End();
-
-        //              timer.Stop ();
-        //                milliseconds = timer.Elapsed.Ticks / 10000f;
     }
 
     private static void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0, bool depthTest = false)
@@ -148,110 +109,11 @@ public class GLDebug : MonoBehaviour
             instance.linesZOff.Add(new Line(start, end, color, Time.time, duration));
     }
 
-    /// <summary>
-    /// Draw a line from start to end with color for a duration of time and with or without depth testing.
-    /// If duration is 0 then the line is rendered 1 frame.
-    /// </summary>
-    /// <param name="start">Point in world space where the line should start.</param>
-    /// <param name="end">Point in world space where the line should end.</param>
-    /// <param name="color">Color of the line.</param>
-    /// <param name="duration">How long the line should be visible for.</param>
-    /// <param name="depthTest">Should the line be obscured by objects closer to the camera ?</param>
     public static void DrawLine(Vector3 start, Vector3 end, Color? color = null, float duration = 0, bool depthTest = false)
     {
         DrawLine(start, end, color ?? Color.white, duration, depthTest);
     }
 
-    /// <summary>
-    /// Draw a line from start to start + dir with color for a duration of time and with or without depth testing.
-    /// If duration is 0 then the ray is rendered 1 frame.
-    /// </summary>
-    /// <param name="start">Point in world space where the ray should start.</param>
-    /// <param name="dir">Direction and length of the ray.</param>
-    /// <param name="color">Color of the ray.</param>
-    /// <param name="duration">How long the ray should be visible for.</param>
-    /// <param name="depthTest">Should the ray be obscured by objects closer to the camera ?</param>
-    public static void DrawRay(Vector3 start, Vector3 dir, Color? color = null, float duration = 0, bool depthTest = false)
-    {
-        if (dir == Vector3.zero)
-            return;
-        DrawLine(start, start + dir, color, duration, depthTest);
-    }
-
-    /// <summary>
-    /// Draw an arrow from start to end with color for a duration of time and with or without depth testing.
-    /// If duration is 0 then the arrow is rendered 1 frame.
-    /// </summary>
-    /// <param name="start">Point in world space where the arrow should start.</param>
-    /// <param name="end">Point in world space where the arrow should end.</param>
-    /// <param name="arrowHeadLength">Length of the 2 lines of the head.</param>
-    /// <param name="arrowHeadAngle">Angle between the main line and each of the 2 smaller lines of the head.</param>
-    /// <param name="color">Color of the arrow.</param>
-    /// <param name="duration">How long the arrow should be visible for.</param>
-    /// <param name="depthTest">Should the arrow be obscured by objects closer to the camera ?</param>
-    public static void DrawLineArrow(Vector3 start, Vector3 end, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20, Color? color = null, float duration = 0, bool depthTest = false)
-    {
-        DrawArrow(start, end - start, arrowHeadLength, arrowHeadAngle, color, duration, depthTest);
-    }
-
-    /// <summary>
-    /// Draw an arrow from start to start + dir with color for a duration of time and with or without depth testing.
-    /// If duration is 0 then the arrow is rendered 1 frame.
-    /// </summary>
-    /// <param name="start">Point in world space where the arrow should start.</param>
-    /// <param name="dir">Direction and length of the arrow.</param>
-    /// <param name="arrowHeadLength">Length of the 2 lines of the head.</param>
-    /// <param name="arrowHeadAngle">Angle between the main line and each of the 2 smaller lines of the head.</param>
-    /// <param name="color">Color of the arrow.</param>
-    /// <param name="duration">How long the arrow should be visible for.</param>
-    /// <param name="depthTest">Should the arrow be obscured by objects closer to the camera ?</param>
-    public static void DrawArrow(Vector3 start, Vector3 dir, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20, Color? color = null, float duration = 0, bool depthTest = false)
-    {
-        if (dir == Vector3.zero)
-            return;
-        DrawRay(start, dir, color, duration, depthTest);
-        Vector3 right = Quaternion.LookRotation(dir) * Quaternion.Euler(0, 180 + arrowHeadAngle, 0) * Vector3.forward;
-        Vector3 left = Quaternion.LookRotation(dir) * Quaternion.Euler(0, 180 - arrowHeadAngle, 0) * Vector3.forward;
-        DrawRay(start + dir, right * arrowHeadLength, color, duration, depthTest);
-        DrawRay(start + dir, left * arrowHeadLength, color, duration, depthTest);
-    }
-
-    /// <summary>
-    /// Draw a square with color for a duration of time and with or without depth testing.
-    /// If duration is 0 then the square is renderer 1 frame.
-    /// </summary>
-    /// <param name="pos">Center of the square in world space.</param>
-    /// <param name="rot">Rotation of the square in euler angles in world space.</param>
-    /// <param name="scale">Size of the square.</param>
-    /// <param name="color">Color of the square.</param>
-    /// <param name="duration">How long the square should be visible for.</param>
-    /// <param name="depthTest">Should the square be obscured by objects closer to the camera ?</param>
-    public static void DrawSquare(Vector3 pos, Vector3? rot = null, Vector3? scale = null, Color? color = null, float duration = 0, bool depthTest = false)
-    {
-        DrawSquare(Matrix4x4.TRS(pos, Quaternion.Euler(rot ?? Vector3.zero), scale ?? Vector3.one), color, duration, depthTest);
-    }
-    /// <summary>
-    /// Draw a square with color for a duration of time and with or without depth testing.
-    /// If duration is 0 then the square is renderer 1 frame.
-    /// </summary>
-    /// <param name="pos">Center of the square in world space.</param>
-    /// <param name="rot">Rotation of the square in world space.</param>
-    /// <param name="scale">Size of the square.</param>
-    /// <param name="color">Color of the square.</param>
-    /// <param name="duration">How long the square should be visible for.</param>
-    /// <param name="depthTest">Should the square be obscured by objects closer to the camera ?</param>
-    public static void DrawSquare(Vector3 pos, Quaternion? rot = null, Vector3? scale = null, Color? color = null, float duration = 0, bool depthTest = false)
-    {
-        DrawSquare(Matrix4x4.TRS(pos, rot ?? Quaternion.identity, scale ?? Vector3.one), color, duration, depthTest);
-    }
-    /// <summary>
-    /// Draw a square with color for a duration of time and with or without depth testing.
-    /// If duration is 0 then the square is renderer 1 frame.
-    /// </summary>
-    /// <param name="matrix">Transformation matrix which represent the square transform.</param>
-    /// <param name="color">Color of the square.</param>
-    /// <param name="duration">How long the square should be visible for.</param>
-    /// <param name="depthTest">Should the square be obscured by objects closer to the camera ?</param>
     public static void DrawSquare(Matrix4x4 matrix, Color? color = null, float duration = 0, bool depthTest = false)
     {
         Vector3
@@ -266,42 +128,11 @@ public class GLDebug : MonoBehaviour
         DrawLine(p_4, p_1, color, duration, depthTest);
     }
 
-    /// <summary>
-    /// Draw a cube with color for a duration of time and with or without depth testing.
-    /// If duration is 0 then the square is renderer 1 frame.
-    /// </summary>
-    /// <param name="pos">Center of the cube in world space.</param>
-    /// <param name="rot">Rotation of the cube in euler angles in world space.</param>
-    /// <param name="scale">Size of the cube.</param>
-    /// <param name="color">Color of the cube.</param>
-    /// <param name="duration">How long the cube should be visible for.</param>
-    /// <param name="depthTest">Should the cube be obscured by objects closer to the camera ?</param>
     public static void DrawCube(Vector3 pos, Vector3? rot = null, Vector3? scale = null, Color? color = null, float duration = 0, bool depthTest = false)
     {
         DrawCube(Matrix4x4.TRS(pos, Quaternion.Euler(rot ?? Vector3.zero), scale ?? Vector3.one), color, duration, depthTest);
     }
-    /// <summary>
-    /// Draw a cube with color for a duration of time and with or without depth testing.
-    /// If duration is 0 then the square is renderer 1 frame.
-    /// </summary>
-    /// <param name="pos">Center of the cube in world space.</param>
-    /// <param name="rot">Rotation of the cube in world space.</param>
-    /// <param name="scale">Size of the cube.</param>
-    /// <param name="color">Color of the cube.</param>
-    /// <param name="duration">How long the cube should be visible for.</param>
-    /// <param name="depthTest">Should the cube be obscured by objects closer to the camera ?</param>
-    public static void DrawCube(Vector3 pos, Quaternion? rot = null, Vector3? scale = null, Color? color = null, float duration = 0, bool depthTest = false)
-    {
-        DrawCube(Matrix4x4.TRS(pos, rot ?? Quaternion.identity, scale ?? Vector3.one), color, duration, depthTest);
-    }
-    /// <summary>
-    /// Draw a cube with color for a duration of time and with or without depth testing.
-    /// If duration is 0 then the square is renderer 1 frame.
-    /// </summary>
-    /// <param name="matrix">Transformation matrix which represent the cube transform.</param>
-    /// <param name="color">Color of the cube.</param>
-    /// <param name="duration">How long the cube should be visible for.</param>
-    /// <param name="depthTest">Should the cube be obscured by objects closer to the camera ?</param>
+
     public static void DrawCube(Matrix4x4 matrix, Color? color = null, float duration = 0, bool depthTest = false)
     {
         Vector3
@@ -330,17 +161,4 @@ public class GLDebug : MonoBehaviour
         DrawLine(up_4, up_1, color, duration, depthTest);
     }
 
-
-    // EXTRAS
-    public static void DrawCircle(Vector3 center, float radius, Color? color = null, float duration = 0, bool depthTest = false)
-    {
-        //            float degRad = Mathf.PI / 180;
-        for (float theta = 0.0f; theta < (2 * Mathf.PI); theta += 0.2f)
-        {
-            Vector3 ci = (new Vector3(Mathf.Cos(theta) * radius + center.x, Mathf.Sin(theta) * radius + center.y, center.z));
-            DrawLine(ci, ci + new Vector3(0, 0.02f, 0), color, duration, depthTest);
-        }
-    }
-
 }
-
